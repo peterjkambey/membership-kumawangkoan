6bd89d803e9c3c2f4ba45d6313944e235986c2e2
PETER KAMBEY <peterjkambey@gmail.com>
2026-06-25 23:01:58 +0700

    Add region seeding to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,17 @@ class DatabaseSeeder extends Seeder
             RolePermissionSeeder::class,
         ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (User::where('email', 'admin@kumawangkoan.org')->doesntExist()) {
+            User::create([
+                'name' => 'Super Admin',
+                'email' => 'admin@kumawangkoan.org',
+                'password' => bcrypt('admin123'),
+            ])->assignRole('super-admin');
+        }
+
+        $regions = ['Wilayah 1', 'Wilayah 2', 'Wilayah 3', 'Wilayah 4', 'Wilayah 5', 'Wilayah 6', 'Wilayah 7'];
+        foreach ($regions as $name) {
+            \App\Models\Region::firstOrCreate(['name' => $name]);
+        }
     }
 }
