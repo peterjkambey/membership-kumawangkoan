da9e46468f1254a472cf0dca8fd22ece988d629e
PETER KAMBEY <peterjkambey@gmail.com>
2026-06-25 23:14:59 +0700

    Redirect root to /admin

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PublicCardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 // E-Membership Card - Public
